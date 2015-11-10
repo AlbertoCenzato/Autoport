49e4aae2fa0319e6997b80c79938fb63fe765bda
Alberto Cenzato <albertocenzato@alice.it>
2015-11-10 14:54:47 +0000

    "p3p_solver" translation completed, erros!!

--- Autoport/Autoport/Autoport.cpp
+++ Autoport/Autoport/Autoport.cpp
@@ -6,6 +6,7 @@
 #include "..\Eigen\Eigen\Dense"
 #include "P3p.h"
 
+using Eigen::Vector3d;
 
 /*
 This function finds the angles (in RADIANS) of the yaw - pitch - roll sequence
@@ -21,9 +22,92 @@ inline double* dcm_to_ypr(Eigen::Matrix3d &R) {
 	ypr[0] = atan(R(0, 1) / R(0, 0));
 	ypr[1] = asin(-R(0, 2));
 	ypr[2] = atan(R(1, 2) / R(2, 2));
+	ypr[0] = 360 * ypr[0] / 2 / M_PI;
+	ypr[1] = 360 * ypr[1] / 2 / M_PI;
+	ypr[2] = 360 * ypr[2] / 2 / M_PI;
 	return ypr;
 }
 
+/* Function che risolve il prp con il metodo di Laurent Kneip
+% Input:
+%   P - Matrice 3x4 dei 4 punti nello spazio assoluto.Le colonne sono i
+%       punti
+%   f - Matrice 3x4 dei 4 versori nel sistema camera che individuano le
+%       direzioni dei quattro punti fissi.Le colonne sono i versori
+% Output :
+%   C - Centro del sistema camera nel sistema di riferimento assoluto
+%   R - Matrice di rotazione dal sistema assoluto a quello camera */
+
+Eigen::Matrix<double, 3, 4> p3p_solver(Eigen::Matrix<double, 3, 4> &P, Eigen::Matrix<double, 3, 4> &f) {
+	//Fixed points at the base station (in millimeters)
+	Vector3d P1 = P.col(0);
+	Vector3d P2 = P.col(1);
+	Vector3d P3 = P.col(2);
+	Vector3d P4 = P.col(3);
+
+	//Versori normati nel riferimento della camera
+	Vector3d f1 = f.col(0);
+	Vector3d f2 = f.col(1);
+	Vector3d f3 = f.col(2);
+	Vector3d f4 = f.col(3);
+
+	//Primo calcolo
+	//Input al codice di Kneip:
+	Eigen::Matrix3d wP;
+	wP << P1, P2, P4; //CHECK IF WORKS LIKE THIS
+	Eigen::Matrix3d iV;
+	iV << f1, f2, f4; //SAME HERE
+
+					  //risoluzione del p3p
+	P3p p3p;
+	Eigen::Matrix<double, 3, 16> poses;	//set n and m dimension
+	p3p.computePoses(iV, wP, poses);
+	Vector3d C1 = poses.col(0);
+	Vector3d C2 = poses.col(4);
+	Vector3d C3 = poses.col(8);
+	Vector3d C4 = poses.col(12);
+	Eigen::Matrix3d R1 = poses.block(0, 1, 3, 3);
+	Eigen::Matrix3d R2 = poses.block(0, 5, 3, 3);
+	Eigen::Matrix3d R3 = poses.block(0, 9, 3, 3);
+	Eigen::Matrix3d R4 = poses.block(0, 13, 3, 3);
+	Eigen::Matrix<double, 3, 4> C;
+	Eigen::Matrix<double, 3, 12> R;
+	C << C1, C2, C3, C4;
+	R << R1, R2, R3, R4;
+
+	//Discriminazione della soluzione esatta
+	Vector3d F31 = (R1*(P3 - C1));
+	Vector3d F32 = (R2*(P3 - C2));
+	Vector3d F33 = (R3*(P3 - C3));
+	Vector3d F34 = (R4*(P3 - C4));
+	F31.norm();
+	F32.norm();
+	F33.norm();
+	F34.norm();
+
+	Eigen::Matrix<double, 4, 1> dF;
+	dF << (F31 - f3).norm(), (F32 - f3).norm(), (F33 - f3).norm(), (F34 - f3).norm();
+	dF = dF.cwiseAbs();
+
+	//find a better algorithm for min finding
+	int min = dF(0);
+	int index = 0;
+	for (int i = 1; i < 4; i++) {
+		if (dF(i) < min) {
+			min = dF(i);
+			index = i;
+		}
+	}
+
+	Vector3d c = C.col(index);
+	Eigen::Matrix3d r = R.block(0, 3 * index, 3, 3);
+
+	Eigen::Matrix<double, 3, 4> solution;
+	solution << c, r;
+
+	//CHECK IF RETURNS BY VALUE OR BY REFERENCE!
+	return solution;
+}
 
 int main()
 {
@@ -31,9 +115,9 @@ int main()
 
 	double focal = 3.46031; // Focale camera[mm]
 
-	Eigen::Vector3d f1 = {  0.08650775,  0.08650775, focal };
-	Eigen::Vector3d f2 = {  0.08650775, -0.08650775, focal };
-	Eigen::Vector3d f3 = { -0.08650775, -0.08650775, focal };
+	Vector3d f1 = {  0.08650775,  0.08650775, focal };
+	Vector3d f2 = {  0.08650775, -0.08650775, focal };
+	Vector3d f3 = { -0.08650775, -0.08650775, focal };
 	f1.normalize();
 	f2.normalize();
 	f3.normalize();
@@ -62,10 +146,31 @@ int main()
 	printf("-------------------------------------------------------\n");
 	printf("\nTEST PROGRAM FOR dcm_to_ypr.cpp");
 	Eigen::Matrix3d R;
-	R << 0.7922, 0.0357, 0.6787, 0.9595, 0.8491, 0.7577, 0.6557, 0.9340, 0.7431;
+	R << 0.8147,0.9134,0.2785,0.9058,0.6324,0.5469,0.1270,0.0975,0.9575;
 	double *ypr = dcm_to_ypr(R);
 	printf("\n\nYaw: %f\nPitch: %f\nRoll: %f", ypr[0], ypr[1], ypr[2]);
 	getchar();
+
+	printf("---------------------------------------------------------\n");
+	printf("\nTEST PROGRAM FOR p3p_solver.cpp");
+	Eigen::Matrix<double, 3, 4> P;
+	P << 0.1626 ,   0.9597 ,   0.2238  ,  0.5060,
+		0.1190  ,  0.3404  ,  0.7513   , 0.6991 ,
+		0.4984  ,  0.5853  ,  0.2551   , 0.8909;
+	Eigen::Matrix<double, 3, 4> f;
+	f << 0.3816, 0.1869, 0.6463, 0.2760,
+		0.7655, 0.4898, 0.7094, 0.6797,
+		0.7952, 0.4456, 0.7547, 0.6551;
+	Eigen::Matrix<double, 3, 4> solution = p3p_solver(P, f);
+	
+	for (int i = 0; i < 3; i++) {
+		printf("\n");
+		for (int j = 0; j < 4; j++)
+			printf("%f ", solution(i, j));
+	}
+
+	getchar();
+
     return 0;
 }
 
