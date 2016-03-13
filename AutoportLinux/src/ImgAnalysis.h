edc93c4bf98e81bea115e10785cd8130d4d48e40
Alberto Cenzato <albertocenzato@alice.it>
2016-03-13 21:50:43 +0000

    Converted ImgAnalysis in a class
    Former-commit-id: b5dbcc0080ad68dcf3be3fdcd33887bce242ab57

--- AutoportLinux/src/ImgAnalysis.h
+++ AutoportLinux/src/ImgAnalysis.h
@@ -1,34 +1,40 @@
 //Copyright (c) 2016 Alberto Cenzato. All rights reserved.
 
+#include <iostream>
+#include <set>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/features2d/features2d.hpp>
 
 using namespace std;
 using namespace cv;
 
-namespace ImgAnalysis {
+#ifndef IMGANALYSIS_H
+#define IMGANALISIS_H
 
-	//--- Functions ---
+class ImgAnalysis {
 
-	Mat filterByColor(Mat &, Scalar &, Scalar &);
-	vector<Point2f> findBlobs(Mat &, SimpleBlobDetector::Params &);
+public:
 
-	vector<Point2f> pattern1(vector<Point2f>&, Mat&);
-	vector<Point2f> pattern3(vector<Point2f>&, Mat&);
-	vector<Point2f> patternMirko(vector<Point2f>&, Mat&, int tolerance);
+	static Mat filterByColor(Mat &img, Scalar &, Scalar &);
+	static vector<Point2f> findBlobs(Mat &img, SimpleBlobDetector::Params &);
+	static vector<Point2f> pattern1(vector<Point2f> &, Mat &);
+	static vector<Point2f> pattern3(vector<Point2f> &, Mat &);
+	static vector<Point2f> patternMirko(vector<Point2f> &, Mat &, int);
 
-	//--- Inline functions ---
+private:
 
-	inline float distancePointToPoint(Point2f &p1, Point2f &p2) {
+	static inline float distancePointToPoint(Point2f &p1, Point2f &p2) {
 		return sqrt(pow(p1.x - p2.x, 2) + pow(p1.y - p2.y, 2));
 	}
 
-	inline void drawDetectedLed(Mat &image, Point2f &keyPoint, string &number) {
+	static inline void drawDetectedLed(Mat &image, Point2f &keyPoint, string &number) {
 		putText(image, number, keyPoint, FONT_HERSHEY_SIMPLEX, 1, Scalar(0, 0, 255),4);
 		imshow("Thresholded Image", image);
 		waitKey(25);
 	}
 
-	inline Point2f centroid(vector<Point2f> &points) {
+	static inline Point2f centroid(vector<Point2f> &points) {
 		float x = 0;
 		float y = 0;
 		for (uint i = 0; i < points.size(); i++) {
@@ -38,6 +44,7 @@ namespace ImgAnalysis {
 		}
 		return Point2f(x / points.size(), y / points.size());
 	}
-}
 
+};
 
+#endif
