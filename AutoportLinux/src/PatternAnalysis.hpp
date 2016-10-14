a49463841442d36369c7fcf29598de6a477f4d5e
alberto <alberto@alberto-Aspire-5755G>
2016-10-14 10:10:12 +0200

    Added comments to PatternAnalysis.hpp
    Former-commit-id: 20f65349c329a8c44be6af6a84aa55e1047ddd15

--- AutoportLinux/src/PatternAnalysis.hpp
+++ AutoportLinux/src/PatternAnalysis.hpp
@@ -17,6 +17,13 @@ using namespace GenPurpFunc;
 
 extern string workingDir;
 
+/*
+ * This class decides if a given vector of 2D points represents
+ * the led pattern or not and, if yes, reorders the points properly
+ * as described by the pattern documentation (see Dropbox > AUTOPORT >
+ * SENSORI MATERIALE > pattern.docx, "ottava geometria")
+ */
+
 class PatternAnalysis {
 
 public:
@@ -34,14 +41,37 @@ public:
 
 	~PatternAnalysis() {}
 
+	/*
+	 * The only one public method. Receives the points detected from the image
+	 * and states if they represent a pattern or not. Decides which
+	 * matching algorithm to use based on the moment: if t = 0
+	 * uses firstPhase, otherwise uses nearestPoints.
+	 *
+	 * @ledPoints: points to evaluate
+	 * @tolerance: temporarly unused parameter
+	 *
+	 * @return: true if a match is found, false otherwise
+	 */
 	bool evaluate(vector<Point2f> &ledPoints, int tolerance);
 
 private:
-	vector<Point2f> oldPoints;
-	vector<Point2f> pattern;
+	// TODO: change with a KD-tree
+	vector<Point2f> oldPoints;	// points at time t-1
+	vector<Point2f> pattern;	// model of the pattern, do not modify
 
 	// TODO: use a KD-tree and simplify the code
-	void nearestPoints(vector<Point2f> &ledPoints, Mat &img, int tolerance) {
+	/*
+	 * WARNING! NOT WORKING by the moment. It was written for another led pattern!
+	 * Receives the points detected from the image
+	 * and states if they represent a pattern or not.
+	 * Used at time t > 0.
+	 *
+	 * @ledPoints: points to evaluate
+	 * @tolerance: temporarly unused parameter
+	 *
+	 * @return: true if a match is found, false otherwise
+	 */
+	bool nearestPoints(vector<Point2f> &ledPoints, Mat &img, int tolerance) {
 
 		vector<Point2f> orderedVector(8);
 
@@ -82,91 +112,24 @@ private:
 		ledPoints.pop_back();
 	}
 
-	// TODO: remove this method
-	// TODO: rename this shitty-named method
+	/*
+	 * Receives the points detected from the image
+	 * and states if they represent a pattern or not.
+	 * Used at time t = 0 (the very first frame)
+	 *
+	 * @ledPoints: points to evaluate
+	 * @tolerance: temporarly unused parameter
+	 *
+	 * @return: true if a match is found, false otherwise
+	 */
 	bool firstPhase(vector<Point2f> &ledPoints, int tolerance) {
 
-		auto size = ledPoints.size();
-		if(size != 5)
-			return false;
-
-		Point2f minPoint1;
-		Point2f minPoint2;
-		float minDist = FLT_MAX;
-
-		vector<Point2f> sortedVector(size);
-
-		for(int i = 0; i < size; i++) {
-			for(int j = 0; j < size; j++) {
-				if(i != j) {
-					float distance = distPoint2Point(ledPoints[i],ledPoints[j]);
-					if(distance < minDist) {
-						minDist = distance;
-						minPoint1 = ledPoints[i];
-						minPoint2 = ledPoints[j];
-					}
-				}
-			}
-		}
-
-		Line line(minPoint1,minPoint2);
-
-		Point2f patternLed3(-1,-1);
-		for(uint i = 0; i < size; i++) {
-			Point2f point = ledPoints[i];
-			if(&point != &minPoint1 && &point != &minPoint2) {
-				float distance = distPoint2Line(point, line);
-				if (distance < tolerance) {
-					patternLed3 = point;
-					break;
-				}
-			}
-		}
-
-		if(patternLed3.x == -1)
-			return false;	//ERRORE!
-
-		sortedVector[3] = patternLed3;
-		float d1 = distPoint2Point(minPoint1,patternLed3);
-		float d2 = distPoint2Point(minPoint2,patternLed3);
-		if(d1 > d2) {
-			sortedVector[1] = minPoint1;
-			sortedVector[2] = minPoint2;
-		}
-		else {
-			sortedVector[1] = minPoint2;
-			sortedVector[2] = minPoint1;
-		}
-
-		Point2f led[2];
-		for(uint i = 0, count = 0; i < size; i++) {
-			Point2f point = ledPoints[i];
-			if(&point != &minPoint1 && &point != &minPoint2 && &point != &patternLed3) {
-				led[count++] = point;
-			}
-		}
-
-		d1 = distPoint2Point(sortedVector[1],led[0]);
-		d2 = distPoint2Point(sortedVector[1],led[1]);
-		if(d1 < d2) {
-			sortedVector[0] = led[0];
-			sortedVector[4] = led[1];
-		}
-		else {
-			sortedVector[0] = led[1];
-			sortedVector[4] = led[0];
-		}
-
-		return true;
-	}
-
-	bool ransac(vector<Point2f> &input) {
-		const uint SIZE = input.size();
+		const uint SIZE = ledPoints.size();
 		assert(SIZE == pattern.size() &&
-			"PatternAnalysis error! RANSAC input vector and pattern vector don't have the same size");
+			"PatternAnalysis error! RANSAC ledPoints vector and pattern vector don't have the same size");
 
-		// find the homography that transforms input points in pattern points
-		Mat_<float> H = findHomography(pattern, input, RANSAC);
+		// find the homography that transforms ledPoints points in pattern points
+		Mat_<float> H = findHomography(pattern, ledPoints, RANSAC);
 
 		if(H.empty()) {
 			cout << "homography not found!" << endl;
@@ -176,13 +139,14 @@ private:
 		// apply the homography to each point
 		vector<Point2f> tmp(SIZE);
 		for(uint i = 0; i < SIZE; ++i) {
-			Vec3f vec(input[i].x, input[i].y, 1);
+			Vec3f vec(ledPoints[i].x, ledPoints[i].y, 1);
 			Mat_<float> p = H*Mat_<float>(vec);
 			Vec3f reversedPoint(p);
 			tmp[i] = Point2f(reversedPoint.val[0],reversedPoint.val[1]);;
 		}
 
 		// find the nearest point
+		// TODO: use a KD-tree
 		int *flag = new int[SIZE];
 		for(uint i = 0; i < SIZE; ++i)
 			flag[i] = -1;
@@ -194,7 +158,7 @@ private:
 				return false;
 			}
 			flag[i] = 1;
-			input[index] = tmp[i];
+			ledPoints[index] = tmp[i];
 		}
 
 		delete [] flag;
